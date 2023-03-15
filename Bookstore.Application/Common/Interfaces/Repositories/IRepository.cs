9a44f523ecd26ae4f9893867c89c86cb8c08aa61
mebry <vadushko@gmail.com>
2023-03-15 16:03:11 +0300

    Add a base repository

---
+++ Bookstore.Application/Common/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookstore.Application.Common.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+
+        Task<T?> GetByIdAsync(int id);
+
+        Task<IEnumerable<T?>> GetAllAsync();
+
+        Task SaveChangesAsync();
+    }
+}
