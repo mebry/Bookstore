3fd409afcfbcefeead6727bb0c8d302212587e63
mebry <vadushko@gmail.com>
2023-04-11 22:33:14 +0300

    Add a new interface(IApplicationDbContext)

---
+++ Bookstore.Application/Common/Interfaces/Context/IApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Bookstore.Application.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Bookstore.Application.Common.Interfaces.Context
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Genre> Genres { get; }
+
+        DbSet<Author> Authors { get; }
+
+        DbSet<Book> Books { get; }
+
+        DbSet<Cart> Carts { get; }
+
+        DbSet<CartDetail> CartDetails { get; }
+
+        DbSet<Order> Orders { get; }
+
+        DbSet<OrderDetail> OrderDetails { get; }
+
+        DbSet<AuthorBook> AuthorsBooks { get; }
+
+        Task SaveChangesAsync();
+    }
+}
