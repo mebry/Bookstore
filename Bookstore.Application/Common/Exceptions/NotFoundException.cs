9afee4400bb9dd7f93b15b6c118469ee85404862
mebry <vadushko@gmail.com>
2023-04-12 01:03:29 +0300

    Add exceptions. Add a new exception (NotFoundException).

---
+++ Bookstore.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookstore.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+            : base() { }
+
+        public NotFoundException(string message)
+            : base(message) { }
+
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" with a key({key}) was not found.") { }
+    }
+}
